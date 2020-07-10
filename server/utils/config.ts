c9bead9ee17fea7c20eaadd991f6d51cbd9e467c
akintunde <jegedeakintunde@gmail.com>
2020-07-10 05:04:43 +0100

    made correction

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -2,4 +2,4 @@ import * as dotenv from "dotenv";
 dotenv.config();
 export const PORT = process.env.PORT || 4000;
 export const DB_CONN_STRING = process.env.DB_CONN_STRING ||   'mongodb://mongo:27017/docker-node-mongo';
-export const DOMAIN = process.env.DOMAIN || "pbidl.io";
+export const DOMAIN = process.env.DOMAIN || "pbid.io";
